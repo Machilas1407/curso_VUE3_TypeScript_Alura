e189bdb89edbdbd34e3796d344c51ec9722a3a0f
machilas <marcello.machilas@gmail.com>
2022-07-27 17:33:35 -0300

    Ajuste de Componentizacao

--- alura-tracker/src/components/BarraLateral.vue
+++ alura-tracker/src/components/BarraLateral.vue
@@ -4,6 +4,22 @@
             <img src="../assets/logo.png" alt="">
         </h1>
         <button class="button" @click="alterarTema">{{ textoDoBotao }}</button>
+        <nav class="panel mt-5">
+            <ul>
+                <li>
+                    <router-link to="/" class="link">
+                        <i class="fas fa-tasks"> </i>
+                        Tarefas
+                    </router-link>
+                </li>
+                <li>
+                    <router-link to="/projetos" class="link">
+                        <i class="fas fa-project-diagram"> </i>
+                        Projetos
+                    </router-link>
+                </li>
+            </ul>
+        </nav>
     </header>
 </template>
 
@@ -51,6 +67,22 @@ header {
         padding: 2.5rem;
         height: auto;
     }
+}
+
+.panel li {
+    margin: 8px 0;
+}
+
+.link {
+    color: #fff;
+}
+
+.link:hover {
+    color: #FAF0CA;
+}
+
+.link.router-link-active {
+    color: #FAF0CA;
 }
 </style>
 
---
+++ alura-tracker/src/views/Tarefas.vue
@@ -0,0 +1,46 @@
+<template>
+
+    <Formulario @aoSalvarTarefa="salvarTarefa" />
+    <div class="lista">
+        <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
+        <Box v-if="listaEstaVazia">
+            Você não está muito produtivo hoje 🫤
+        </Box>
+    </div>
+
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from "vue";
+import Formulario from "../components/Form.vue";
+import Tarefa from "../components/Tarefa.vue";
+import Box from "../components/Box.vue";
+import ITarefa from '../interfaces/ITarefa'
+
+export default defineComponent({
+    name: 'App',
+    components: {
+        Formulario,
+        Tarefa,
+        Box
+    },
+    data() {
+        return {
+            tarefas: [] as ITarefa[]
+        }
+    },
+    computed: {
+        listaEstaVazia(): boolean {
+            return this.tarefas.length === 0
+        }
+    },
+    methods: {
+        salvarTarefa(tarefa: ITarefa) {
+            this.tarefas.push(tarefa)
+        },
+    }
+
+});
+</script>
+
