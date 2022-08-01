a0e4ebe1e537544b5e49f3453852fc58a25d7ca4
machilas <marcello.machilas@gmail.com>
2022-08-01 12:12:37 -0300

    Tarefas

--- alura-tracker/src/views/Tarefas.vue
+++ alura-tracker/src/views/Tarefas.vue
@@ -2,24 +2,24 @@
 
     <Formulario @aoSalvarTarefa="salvarTarefa" />
     <div class="lista">
-        <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" />
+        <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" @aoTarefaClicada="selecionarTarefa" />
         <Box v-if="listaEstaVazia">
             Você não está muito produtivo hoje 🫤
         </Box>
 
-        <div class="modal">
+        <div class="modal" :class="{ 'is-active': tarefaSelecionada }">
             <div class="modal-background"></div>
             <div class="modal-card">
                 <header class="modal-card-head">
-                    <p class="modal-card-title">Modal title</p>
-                    <button class="delete" aria-label="close"></button>
+                    <p class="modal-card-title">Editando uma tarefa</p>
+                    <button @click="fecharModal()" class="delete" aria-label="close"></button>
                 </header>
                 <section class="modal-card-body">
                     <!-- Content ... -->
                 </section>
                 <footer class="modal-card-foot">
-                    <button class="button is-success">Save changes</button>
-                    <button class="button">Cancel</button>
+                    <button class="button is-success">Salvar Alterações</button>
+                    <button @click="fecharModal()" class="button">Cancelar</button>
                 </footer>
             </div>
         </div>
@@ -44,15 +44,27 @@ export default defineComponent({
         Tarefa,
         Box
     },
+    data() {
+        return {
+            tarefaSelecionada: null as ITarefa | null
+        }
+    },
     computed: {
         listaEstaVazia(): boolean {
             return this.tarefas.length === 0
-        }
+        },
+
     },
     methods: {
         salvarTarefa(tarefa: ITarefa): void {
             this.store.dispatch(CADASTRAR_TAREFA, tarefa)
         },
+        selecionarTarefa(tarefa: ITarefa) {
+            this.tarefaSelecionada = tarefa
+        },
+        fecharModal() {
+            this.tarefaSelecionada = null
+        }
     }
     ,
     setup() {
